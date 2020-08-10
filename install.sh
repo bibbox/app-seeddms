eb437ff569660e8543ec896e4234cce775cd432d
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-08-10 09:57:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir -p data/var/lib/mysql
-mkdir -p data/var/www/seeddms51x/data/lucene
-mkdir -p data/var/www/seeddms51x/data/staging
+mkdir -p data/var/www/seeddms60x/data/lucene
+mkdir -p data/var/www/seeddms60x/data/staging
 chmod -R 777 data
 docker-compose up -d
