f287e2f89ea13404c4a4100e2de6a8e2bf4a801c
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-09-03 12:48:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ mkdir -p data/var/lib/mysql
 mkdir -p data/var/www/seeddms60x/data/lucene
 mkdir -p data/var/www/seeddms60x/data/staging
 chmod -R 777 data
+
+sudo docker network create bibbox-default-network
+
 docker-compose up -d
