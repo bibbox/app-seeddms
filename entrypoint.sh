ae17d68c3c39a8331e9ff0f5979ede425f123ad1
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-10-28 16:55:25 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,8 @@ if [ ! -f $file ] ; then
   chown -R www-data:www-data /var/www/seeddms60x/www/seeddms777/
 
   #tail -f /var/log/dpkg.log
-  service apache2 start
-  tail -f /var/log/dpkg.log
   mkdir /opt/dist/
   touch /opt/dist/deployed.done
+  service apache2 start
+  tail -f /var/log/dpkg.log
 fi
