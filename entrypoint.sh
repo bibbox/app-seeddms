5dd5b3d888689cd157963a0bca2675a42289571e
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-10-28 15:05:03 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-mkdir /var/www/seeddms60x/www/seeddms7/
-cp -R /var/www/seeddms60x/seeddms/* seeddms7/
-chown -R /var/www/seeddms60x/www/seeddms7/
+mkdir /var/www/seeddms60x/www/seeddms777/
+cp -R /var/www/seeddms60x/seeddms/* seeddms777/
+chown -R /var/www/seeddms60x/www/seeddms777/
