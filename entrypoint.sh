35664f5f6db2ab48865378209949ce412dc4078c
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-10-28 16:40:11 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 mkdir /var/www/seeddms60x/www/seeddms777/
 cp -R /var/www/seeddms60x/seeddms/* /var/www/seeddms60x/www/seeddms777/
-chown -R www-data /var/www/seeddms60x/www/seeddms777/
+chown -R www-data:www-data /var/www/seeddms60x/www/seeddms777/
 
 #tail -f /var/log/dpkg.log
 service apache2 start
