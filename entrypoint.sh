78a2a90b81a4b02db7d5bc20ad05e6f255a6b964
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-10-28 15:50:50 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ mkdir /var/www/seeddms60x/www/seeddms777/
 cp -R /var/www/seeddms60x/seeddms/* /var/www/seeddms60x/www/seeddms777/
 chown -R www-data /var/www/seeddms60x/www/seeddms777/
 
-tail -f /var/log/dpkg.log
+#tail -f /var/log/dpkg.log
