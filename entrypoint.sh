8e6cbea314aeac6404eb78219f76ba4a26d845df
stefanherdy <69188729+stefanherdy@users.noreply.github.com>
2020-11-10 11:16:01 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 file=/opt/dist/deployed.done
 if [ ! -f $file ] ; then
-  mkdir /var/www/seeddms60x/www/seeddms777/
-  cp -R /var/www/seeddms60x/seeddms/* /var/www/seeddms60x/www/seeddms777/
-  chown -R www-data:www-data /var/www/seeddms60x/www/seeddms777/
+  mkdir /var/www/seeddms60x/www/§§INSTANCENAME/
+  cp -R /var/www/seeddms60x/seeddms/* /var/www/seeddms60x/www/§§INSTANCENAME/
+  chown -R www-data:www-data /var/www/seeddms60x/www/§§INSTANCENAME/
 
   #tail -f /var/log/dpkg.log
   mkdir /opt/dist/
